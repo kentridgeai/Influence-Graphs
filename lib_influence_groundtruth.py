e88fc2a167b143079217e6b537a91ca48e6e7dae
Dailonic <dillydallytht@gmail.com>
2025-07-23 11:16:20 +0800

    bruh..

--- lib_influence_groundtruth.py
+++ lib_influence_groundtruth.py
@@ -69,12 +69,13 @@ def update_IG_GT(IG, main_model, batch_indices, old_trainloss, IG_trainloader, t
                     loss = criterion(outputs, labels.long())
     
                 trainloss[indices] = loss.detach()
-            
+
+    # Convert trainloss for numpy operations
+    trainloss = trainloss.cpu().numpy()
     batchloss_diff = old_batchloss - trainloss[batch_indices]
     trainloss_diff = old_trainloss - trainloss 
     
     scale_ref      = copy.copy(trainloss_diff)
-    scale_ref      = scale_ref.cpu().numpy()
     scale_ref      = np.sqrt(np.mean(scale_ref**2)) 
     batchloss_diff = batchloss_diff / scale_ref
     trainloss_diff = trainloss_diff / scale_ref
